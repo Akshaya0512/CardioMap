60f00915dc95b2915bba6bc54d9cab3722ee3786
Akshaya0512 <akvelvadapu@gmail.com>
2026-04-19 17:45:29 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+
+    print("CardioMap Pipeline Running...")
+
+    print("Step 1: Data loading")
+    print("Step 2: Preprocessing")
+    print("Step 3: Model training")
+    print("Step 4: Evaluation")
+    print("Step 5: Results saved in /results")
+
+    print("Pipeline completed.")
+
+
+if __name__ == "__main__":
+    main()
