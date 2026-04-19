ac40ae1ffdb9af428cc5e2b901ae99fe6f85fc8a
Akshaya0512 <akvelvadapu@gmail.com>
2026-04-19 17:50:35 -0500

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+
+def load_data(ems_path, hospital_path):
+
+    ems = pd.read_excel(ems_path)
+    hospitals = pd.read_excel(hospital_path)
+
+    print("EMS shape:", ems.shape)
+    print("Hospitals shape:", hospitals.shape)
+
+    return ems, hospitals
